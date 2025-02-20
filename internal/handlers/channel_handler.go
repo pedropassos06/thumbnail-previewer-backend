676538573436b1021f231587778d73a2a4b1440d
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-19 21:26:48 -0500

    feat: modify GetChannelHandler to utilize LRU cache for improved performance

--- internal/handlers/channel_handler.go
+++ internal/handlers/channel_handler.go
@@ -3,22 +3,25 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/golang/groupcache/lru"
 	"github.com/pedropassos06/thumbnail-previewer-backend/internal/responses"
 	"github.com/pedropassos06/thumbnail-previewer-backend/internal/services"
 )
 
-func GetChannelHandler(w http.ResponseWriter, r *http.Request) {
-	channelHandle := r.URL.Query().Get("handle")
-	if channelHandle == "" {
-		responses.SendError(w, http.StatusBadRequest, "channel handle is required")
-		return
-	}
+func GetChannelHandler(cache *lru.Cache) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		channelHandle := r.URL.Query().Get("handle")
+		if channelHandle == "" {
+			responses.SendError(w, http.StatusBadRequest, "channel handle is required")
+			return
+		}
 
-	channel, err := services.FetchChannel(channelHandle)
-	if err != nil {
-		responses.SendError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
+		channel, err := services.FetchChannel(channelHandle, cache)
+		if err != nil {
+			responses.SendError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
 
-	responses.SendSuccess(w, channel)
+		responses.SendSuccess(w, channel)
+	}
 }
