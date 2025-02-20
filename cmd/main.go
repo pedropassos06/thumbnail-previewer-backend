a5a78f7ce3ddff2b629a3fa1c08b5d5d80eb2f00
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-19 21:26:43 -0500

    feat: integrate LRU cache and initialize router in main function

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/golang/groupcache/lru"
 	"github.com/pedropassos06/thumbnail-previewer-backend/internal/config"
 	"github.com/pedropassos06/thumbnail-previewer-backend/internal/router"
 )
 
 func init() {
 	config.LoadEnv()
+
 }
 
 func main() {
 	fmt.Println("Starting server...")
-	router.StartServer()
+
+	// Initialize LRU cache
+	cache := lru.New(1000)
+
+	// Initialize router with cache
+	r := chi.NewRouter()
+	router.InitRoutes(r, cache)
+
+	// Get port from env
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		log.Fatalf("PORT not set in .env file")
+	}
+
+	log.Printf("Starting server on port %s", port)
+	http.ListenAndServe(":"+port, r)
 }
