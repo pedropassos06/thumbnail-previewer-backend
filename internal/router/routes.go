2962063b60454545e2551f73eca489a55415a1d9
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-19 21:26:52 -0500

    feat: update InitRoutes to accept LRU cache for GetChannelHandler

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -1,33 +1,14 @@
 package router
 
 import (
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/go-chi/chi/v5"
+	"github.com/golang/groupcache/lru"
 	"github.com/pedropassos06/thumbnail-previewer-backend/internal/handlers"
 	"github.com/pedropassos06/thumbnail-previewer-backend/internal/middleware"
 )
 
-func InitRoutes(r *chi.Mux) {
+func InitRoutes(r *chi.Mux, cache *lru.Cache) {
 	r.Use(middleware.CorsMiddleware)
 	r.Get("/channel/ping", handlers.PingHandler)
-	r.Get("/channel", handlers.GetChannelHandler)
-}
-
-func StartServer() {
-	// init new router and routes
-	r := chi.NewRouter()
-	InitRoutes(r)
-
-	// get port from env
-	port := os.Getenv("SERVER_PORT")
-	if port == "" {
-		log.Fatalf("PORT not set in .env file")
-	}
-
-	log.Printf("Starting server on port %s", port)
-	// start server
-	http.ListenAndServe(":"+port, r)
+	r.Get("/channel", handlers.GetChannelHandler(cache))
 }
