773558575e20e773dac7f49c55054c32319ff3f8
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-22 10:51:40 -0500

    feat: add rate limiting to /channel route using RateLimitMiddleware

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -1,14 +1,23 @@
 package router
 
 import (
+	"time"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/golang/groupcache/lru"
 	"github.com/pedropassos06/thumbnail-previewer-backend/internal/handlers"
 	"github.com/pedropassos06/thumbnail-previewer-backend/internal/middleware"
+	"golang.org/x/time/rate"
 )
 
 func InitRoutes(r *chi.Mux, cache *lru.Cache) {
 	r.Use(middleware.CorsMiddleware)
 	r.Get("/channel/ping", handlers.PingHandler)
-	r.Get("/channel", handlers.GetChannelHandler(cache))
+
+	// Initialize rate limiter
+	limiter := rate.NewLimiter(rate.Every(time.Minute/3), 3)
+
+	// Initialize route with rate limiter
+	r.With(middleware.RateLimitMiddleware(limiter)).Get("/channel", handlers.GetChannelHandler(cache))
+
 }
