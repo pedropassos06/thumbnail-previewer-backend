faecae4573e688d85985592f615ebea2abd02fa9
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-23 18:09:11 -0500

    feat: update CORS middleware to use environment variable for frontend URL

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,10 +1,14 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") // frontend URL
+		frontendUrl := os.Getenv("FRONTEND_URL")
+		w.Header().Set("Access-Control-Allow-Origin", frontendUrl)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
