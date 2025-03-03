feb8240422919a28dc1b67ba5e3812138a87596c
Pedro Passos <pedropassos.yt@gmail.com>
2025-03-03 17:09:40 -0500

    refactor: simplify FetchChannel function to make more readable

--- internal/services/youtube_service.go
+++ internal/services/youtube_service.go
@@ -10,20 +10,42 @@ import (
 	"github.com/pedropassos06/thumbnail-previewer-backend/internal/responses"
 )
 
-type YoutubeAPIConsumer interface {
-	FetchChannel(handle string) (*models.Channel, error)
-}
-
 func FetchChannel(handle string) (*models.Channel, error) {
-	apiKey := os.Getenv("YOUTUBE_API_KEY")
-	if apiKey == "" {
-		return nil, fmt.Errorf("YOUTUBE_API_KEY is not set")
+	apiKey, err := getAPIKey()
+	if err != nil {
+		return nil, err
 	}
 
-	// Makes a single request to fetch both the channel name and profile picture,
-	// reducing unnecessary API calls and improving performance.
-	url := fmt.Sprintf("https://www.googleapis.com/youtube/v3/channels?part=snippet&fields=items/snippet/title,items/snippet/thumbnails/default&key=%s&forHandle=%s", apiKey, handle)
+	url := buildURL(apiKey, handle)
+	channel, err := callYoutubeAPI(url)
+	if err != nil {
+		return nil, err
+	}
 
+	if len(channel.Items) == 0 {
+		return nil, fmt.Errorf("channel not found")
+	}
+
+	return models.NewChannel(
+		channel.Items[0].Snippet.Title,
+		channel.Items[0].Snippet.Thumbnails.Default.URL,
+	), nil
+}
+
+func getAPIKey() (string, error) {
+	apiKey := os.Getenv("YOUTUBE_API_KEY")
+	if apiKey == "" {
+		return "", fmt.Errorf("YOUTUBE_API_KEY is not set")
+	}
+
+	return apiKey, nil
+}
+
+func buildURL(apiKey, handle string) string {
+	return fmt.Sprintf("https://www.googleapis.com/youtube/v3/channels?part=snippet&fields=items/snippet/title,items/snippet/thumbnails/default&key=%s&forHandle=%s", apiKey, handle)
+}
+
+func callYoutubeAPI(url string) (*responses.YoutubeResponse, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch channel: %w", err)
@@ -39,14 +61,5 @@ func FetchChannel(handle string) (*models.Channel, error) {
 		return nil, fmt.Errorf("failed to decode youtube response: %w", err)
 	}
 
-	if len(result.Items) == 0 {
-		return nil, fmt.Errorf("channel not found")
-	}
-
-	channel := models.NewChannel(
-		result.Items[0].Snippet.Title,
-		result.Items[0].Snippet.Thumbnails.Default.URL,
-	)
-
-	return channel, nil
+	return &result, nil
 }
